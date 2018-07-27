9960bf57de527ed520fb3951b69b30648c61dd9a
Hieu <vuminhhieu1805@gmail.com>
2018-07-27 20:29:59 +0700

    UPDATE font error

--- App.js
+++ App.js
@@ -2,19 +2,40 @@ import React from 'react';
 import { createStackNavigator } from 'react-navigation';
 import ListMovies from './listMovies';
 import MovieItem from './movieItem';
+import {Container} from 'native-base';
 
 const RootStack = createStackNavigator(
-  {
-    ListMovies: { screen: ListMovies, navigationOptions: { header: null } },
-    MovieItem: MovieItem
-  },
-  {
-    initialRouteName: 'ListMovies',
-  }
+	{
+		ListMovies: { screen: ListMovies, navigationOptions: { header: null } },
+		MovieItem: MovieItem
+	},
+	{
+		initialRouteName: 'ListMovies',
+	}
 );
 
 export default class App extends React.Component {
-  render() {
-    return <RootStack />;
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			font_loading : true,
+		}
+	}
+	async componentWillMount() {
+		await Expo.Font.loadAsync({
+			'Ionicons': require('native-base/Fonts/Ionicons.ttf'),
+		});
+		this.setState({
+			font_loading: false,
+		});
+	}
+
+	render() {
+		if ( !this.state.font_loading ) {
+			return <RootStack/>;
+		}
+		else {
+			return <Container/>;
+		}
+	}
 }
